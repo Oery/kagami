beda5a0bd7a16570eab0182b4b4495101eb1f173
Oery <oery.dev@gmail.com>
2024-10-21 15:18:12 +0200

    Add Macro to get origin from packets

--- src/tcp/origin.rs
+++ src/tcp/origin.rs
@@ -4,6 +4,18 @@ pub enum Origin {
     Server,
 }
 
+impl Origin {
+    pub fn from_module_path(module: &str) -> Origin {
+        if module.contains("client") {
+            return Origin::Client;
+        } else if module.contains("server") {
+            return Origin::Server;
+        }
+
+        panic!("A Packet must be defined in either a client or server module");
+    }
+}
+
 impl std::fmt::Display for Origin {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let s = match self {
