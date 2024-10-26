05525d4ab826068c163ff2b63dd65d961986b344
Oery <oery.dev@gmail.com>
2024-10-26 17:50:06 +0200

    Add Send for Context

--- src/tcp/context.rs
+++ src/tcp/context.rs
@@ -1,33 +1,68 @@
-use crate::minecraft::{ClientPacket, Packets, ServerPacket};
+use crate::minecraft::{ClientPacket, Packet, Packets, ServerPacket};
 use std::io::{Error, ErrorKind};
-use std::sync::mpsc::Sender;
+use tokio::sync::mpsc::Sender;
 
 pub struct Context<'a, T = Packets> {
     pub packet: &'a mut T,
-    pub client: Client,
-    pub server: Server,
+    pub client: &'a Client,
+    pub server: &'a Server,
 }
 
+#[derive(Clone, Debug)]
 pub struct Client {
-    pub tx: Sender<Vec<u8>>,
+    tx: Sender<Vec<u8>>,
 }
 
 impl Client {
-    pub fn send<T: ClientPacket>(&self, packet: &T) -> std::io::Result<()> {
-        self.tx
-            .send(packet.serialize_packet()?)
-            .map_err(|e| Error::new(ErrorKind::Other, e))
+    pub fn new(tx: &Sender<Vec<u8>>) -> Self {
+        Self { tx: tx.clone() }
+    }
+
+    pub fn send<T: ServerPacket>(&self, packet: T) {
+        let raw_packet = match packet.serialize_packet() {
+            Ok(raw_packet) => raw_packet,
+            Err(e) => {
+                eprintln!("Failed to serialize packet: {:?}", e);
+                return;
+            }
+        };
+
+        let tx = self.tx.clone();
+        tokio::spawn(async move {
+            tx.send([raw_packet.0, raw_packet.1].concat())
+                .await
+                .map_err(|e| Error::new(ErrorKind::Other, e))
+        });
     }
 }
 
+#[derive(Clone, Debug)]
 pub struct Server {
-    pub tx: Sender<Vec<u8>>,
+    tx: Sender<Vec<u8>>,
 }
 
 impl Server {
-    pub fn send<T: ServerPacket>(&self, packet: &T) -> std::io::Result<()> {
-        self.tx
-            .send(packet.serialize_packet()?)
-            .map_err(|e| Error::new(ErrorKind::Other, e))
+    pub fn new(tx: &Sender<Vec<u8>>) -> Self {
+        Self { tx: tx.clone() }
+    }
+
+    pub fn send<T: ClientPacket>(&self, packet: T) {
+        let raw_packet = match packet.serialize_packet() {
+            Ok(raw_packet) => raw_packet,
+            Err(e) => {
+                eprintln!("Failed to serialize packet: {:?}", e);
+                return;
+            }
+        };
+
+        let tx = self.tx.clone();
+        tokio::spawn(async move {
+            tx.send([raw_packet.0, raw_packet.1].concat())
+                .await
+                .map_err(|e| Error::new(ErrorKind::Other, e))
+        });
+    }
+}
+
     }
 }
